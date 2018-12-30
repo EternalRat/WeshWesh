68d27ee2ea48fd08a8498575b4e54bbce9ab1eee
EternalRat <44569175+EternalRat@users.noreply.github.com>
2018-12-31 00:30:56 +0100

    Update record.js

--- cmds/admin/record.js
+++ cmds/admin/record.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the right to do this !");
+
     let guinnesschannel = message.guild.channels.find('name', 'guinness-version-discord')
     if (!guinnesschannel) return message.channel.send("Missing the channel guinness-version-discord")
 

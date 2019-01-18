a0b7f2601e78d77db8c79f72a3603ad0e3db8a09
EternalRat <44569175+EternalRat@users.noreply.github.com>
2019-01-18 20:46:12 +0100

    Update avatarserv.js

--- cmds/admin/avatarserv.js
+++ cmds/admin/avatarserv.js
@@ -1,1 +1,17 @@
+const Discord = require("discord.js")
 
+module.exports.run = async (bot, message, args) => {
+    let msg = await message.channel.send("Generating Avatar...");
+    let target = message.mentions.users.first(
+    let embed = new Discord.RichEmbed()
+        .setDescription(`${message.author} here is the avatar of ${message.guild.name}`)
+        .setImage(message.guild.iconURL)
+        .setFooter(`Copyright - ${bot.user.username}`)
+    message.channel.send(embed)
+  msg.delete();
+}
+
+module.exports.help = {
+    name: "avatarserv",
+    alias: "as"
+}

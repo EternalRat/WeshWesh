1b77ac4613453f9efe6178262891f00578a8a4bc
EternalRat <44569175+EternalRat@users.noreply.github.com>
2019-01-27 20:24:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,8 +71,8 @@ bot.on("ready", async () => {
         var m = date.getMinutes()
         console.log("Bip")
         const msgchannel = bot.channels.find(channel => channel.name === "discussion")
-        if(h === 20) {
-            if(m === 05) {
+        if(h === 19) {
+            if(m === 25) {
             let embeded = new Discord.RichEmbed()
                 .addField("Nous espérons que vous aimez le serveur !", "Si c'est le cas n'hésitez surtout pas à le partager à vos amis !")
                 .setThumbnail(bot.guild.iconURL)

ec147e8726a35430fd4de24225a8623e91271c2e
EternalRat <44569175+EternalRat@users.noreply.github.com>
2019-02-03 19:30:49 +0100

    Update kill.js

--- cmds/admin/kill.js
+++ cmds/admin/kill.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 module.exports.run = async(message, bot) => {
   let author = message.mentions.users
   
-  message.channel.send(`${message.author} has killed ${author})
+  message.channel.send(`${message.author} has killed ${author}`)
 }
 
 module.exports.help = {

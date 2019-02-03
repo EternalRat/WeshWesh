757dbdaa3fe71bdfb47ee8c478bddf3bc84373fb
EternalRat <44569175+EternalRat@users.noreply.github.com>
2019-02-03 19:28:05 +0100

    Create kill.js

---
+++ cmds/admin/kill.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+
+module.exports.run = async(message, bot) => {
+  let author = message.mentions.users
+  
+  message.channel.send(`${message.author} has killed ${author})
+}
+
+module.exports.help = {
+  name: "kill"
+}

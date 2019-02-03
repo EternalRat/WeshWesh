3ec87cddae8a2825b3a0512545efccc4c4eef760
EternalRat <44569175+EternalRat@users.noreply.github.com>
2019-02-03 19:47:21 +0100

    Update kill.js

--- cmds/admin/kill.js
+++ cmds/admin/kill.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async(bot, message, args) => {
-  let target = message.guild.members.get("348530295437525002").name
+  let target = message.guild.members.get("348530295437525002").username
   
   message.channel.send(`${message.author} has killed ${target}`)
 }
